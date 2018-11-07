2c6e0d51303038108f2d10e0f0488c66eaa6b270
Gareth Evans <garethjevans@users.noreply.github.com>
2018-11-07 16:55:29 +0000

    feat: support different helm installations

--- build.sh
+++ build.sh
@@ -3,22 +3,37 @@
 set -euo pipefail
 
 SA=~/.gke_sa.json
-HELM_VERSION=2.10.0
+HELM_VERSION=2.11.0
+HELM=helm
+
+function init() {
+    mkdir -p ~/.jx/bin
+    export PATH=$PATH:~/.jx/bin
+}
 
 function install_dependencies() {
-	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
-	tar xvf jx-linux-amd64.tar.gz
-	rm jx-linux-amd64.tar.gz
+    wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
+    tar xvf jx-linux-amd64.tar.gz
+    rm jx-linux-amd64.tar.gz
+    mv jx ~/.jx/bin
 
-	mkdir -p ~/.jx/bin
-
- 	wget https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz	
- 	tar xvf helm-v${HELM_VERSION}-linux-amd64.tar.gz	
- 	rm helm-v${HELM_VERSION}-linux-amd64.tar.gz	
- 	mv linux-amd64/helm ~/.jx/bin
-	rm -fr linux-amd64
-	
-	~/.jx/bin/helm init --client-only
+    if [ "true" == "${HELM3}" ]; then
+        echo "Installing helm3"
+        wget https://github.com/jstrachan/helm/releases/download/untagged-93375777c6644a452a64/helm-linux-amd64.tar.gz
+        tar xvf helm-linux-amd64.tar.gz
+        rm helm-linux-amd64.tar.gz
+        mv helm3 ~/.jx/bin/
+        ln -s ~/.jx/bin/helm3 ~/.jx/bin/helm
+        ${HELM} init
+    else
+        echo "Installing ${HELM_VERSION}"
+        wget https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz
+        tar xvf helm-v${HELM_VERSION}-linux-amd64.tar.gz
+        rm helm-v${HELM_VERSION}-linux-amd64.tar.gz
+        mv linux-amd64/helm ~/.jx/bin
+        rm -fr linux-amd64
+        ${HELM} init --client-only
+    fi
 }
 
 function configure_environment() {
@@ -37,17 +52,24 @@ function apply() {
 	IFS=$OLDIFS
 
 	git status
-
+	which jx
 	
 	if [[ "${CI_BRANCH}" == "master" ]]; then
 		echo "Running master build"
-		./jx create terraform --verbose ${CLUSTER_COMMAND} -b --install-dependencies -o ${ORG} --gke-service-account ${SA}
+		jx create terraform --verbose ${CLUSTER_COMMAND} \
+		    -b --install-dependencies -o ${ORG} \
+		    --gke-service-account ${SA}
 	else
 		echo "Running PR build for ${CI_BRANCH}"
-		./jx create terraform --verbose ${CLUSTER_COMMAND} -b --install-dependencies -o ${ORG} --gke-service-account ${SA} --skip-terraform-apply --local-organisation-repository .
+		jx create terraform --verbose ${CLUSTER_COMMAND} \
+		     -b --install-dependencies -o ${ORG} \
+		     --gke-service-account ${SA} \
+		     --skip-terraform-apply \ 
+		     --local-organisation-repository .
 	fi
 }
 
+init
 install_dependencies
 configure_environment
 apply
